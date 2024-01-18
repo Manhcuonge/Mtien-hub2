223ceac375a83592a8a6f55ec4483aa103f48893
Manhcuonge <157036803+Manhcuonge@users.noreply.github.com>
2024-01-18 22:17:18 +0700

    Add files via upload

---
+++ obf_OotdC7L5oGMn30f353zCd9UK58ZOZtW4T48BJM1Q493ExDlhTdCS84v7LEsN51ZR.lua
@@ -0,0 +1,3 @@
+_G.Name = "NOWVIP" -- Put You Name Script Here
+_G.Logo = "u3vit3" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
